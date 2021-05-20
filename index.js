0e12e2a5a6ec13069533f0a5b331722a299f390b
lycho33 <cho.lyds@gmail.com>
2021-05-20 15:18:56 -0400

    commit tree 741c3c9882f7ca2ed900721ca5f31622194d3f53

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+class Polygon {
+    constructor(sides){
+        this.sides = sides;
+        console.log(sides)
+    }
+
+    get countSides(){
+        return this.sides.length
+    }
+
+    get perimeter() {
+        let sum = 0
+        for (var int of this.sides) {
+            sum += int
+        }
+        return sum
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid() {
+        let side1 = this.sides[0]
+        let side2 = this.sides[1]
+        let side3 = this.sides[2]
+
+        if ((side1 + side2 > side3) && (side2 + side3 > side1) && (side1 + side3 > side2)){
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        let side1 = this.sides[0]
+        let side2 = this.sides[1]
+        let side3 = this.sides[2]
+        let side4 = this.sides[3]
+
+        if ((side1 === side2) && (side1 === side3) && (side1 === side4) && (side2 === side3) && (side2 === side4) && (side3 === side4)) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    get area(){
+        return (this.sides[0] ** 2)
+    }
+}
